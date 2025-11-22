157440ca91c453c33e7c4cd3d06eabf6e5080366
Amira Alzaian <amira23204@gmail.com>
2025-11-23 00:06:03 +0200

    set

--- src/JS/firebase/firebase.js
+++ src/JS/firebase/firebase.js
@@ -4,9 +4,7 @@ import { getAuth } from "firebase/auth";
 
 import { firebaseConfig } from "./firebase.config.js";
 
-// init
 const app = initializeApp(firebaseConfig);
 
-// services
 export const db = getFirestore(app);
 export const auth = getAuth(app);
