3b1d7d6bf594d61578754a90d256c5b68b655f96
batoulalatrsh <batoulalatrsh76@gmail.com>
2025-11-26 02:19:28 +0200

    implement admin structure

--- src/JS/componentsJS/sidebar.js
+++ src/JS/componentsJS/sidebar.js
@@ -2,11 +2,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const sidebarEl = document.querySelector("#sidebar");
   if (!sidebarEl) return;
 
+  const design = detectSidebar();
+
+  const c = `
+      <aside class="hidden lg:block col-span-0 lg:col-span-1 bg-white px-6 py-8">
+      ${design}
+      </aside>
+      `;
+  sidebarEl.innerHTML = c;
+
+  activeCurrentPage();
+});
+
+export const detectSidebar = function () {
   const role = localStorage.getItem("role"); // "student" or "instructor"
 
   // STUDENT SIDEBAR
   const studentSidebar = `
-    <aside class="hidden lg:block col-span-0 lg:col-span-1 bg-white px-6 py-8">
+    
       <nav class="flex flex-col gap-6">
 
         <div>
@@ -38,27 +51,26 @@ document.addEventListener("DOMContentLoaded", () => {
           Logout
         </a>
       </nav>
-    </aside>
+    
   `;
 
   // INSTRUCTOR SIDEBAR
   const instructorSidebar = `
-    <aside class="hidden lg:block col-span-0 lg:col-span-1 bg-white px-6 py-8">
-      <nav class="flex flex-col gap-6">
+         <nav class="flex flex-col gap-6">
 
-        <a href="../../Pages/student/dashboard.html" class="nav-link">
+        <a href="../../Pages/student/dashboard.html" class="dashboard-btn nav-link">
           <i class="fa-solid fa-chart-line"></i> Dashboard
         </a>
 
-        <a href="../../Pages/instructor/instructor-courses.html" class="nav-link">
+        <a href="../../Pages/instructor/instructorCourses.html" class="instructor-btn nav-link">
           <i class="fa-solid fa-book"></i> My Courses
         </a>
 
-        <a href="../../Pages/instructor/mark-attendance.html" class="nav-link">
+        <a href="../../Pages/instructor/markAttendance.html" class="mark-btn nav-link">
           <i class="fa-solid fa-clipboard-check"></i> Mark Attendance
         </a>
 
-        <a href="../../Pages/instructor/course-reports.html" class="nav-link ">
+        <a href="../../Pages/instructor/courseReports.html" class="course-btn nav-link ">
           <i class="fa-solid fa-chart-pie"></i> Course Reports
         </a>
 
@@ -67,68 +79,66 @@ document.addEventListener("DOMContentLoaded", () => {
           Logout
         </a>
       </nav>
-    </aside>
+    
   `;
   const adminSidebar = `
-  <aside
-  class="hidden lg:block col-span-0 lg:col-span-1 bg-white px-6 py-8"
->
-  <!-- Menu -->
-  <nav class="space-y-2">
-    <a href="admin-dashboard.html"
-      class="nav-link">
-      <i class="fa-solid fa-chart-line text-primary"></i>
-      Dashboard
-    </a>
+     
+      <!-- Menu -->
+      <nav class="space-y-2">
+        <a href="admin-dashboard.html"
+          class="dashboard-btn nav-link">
+          <i class="fa-solid fa-chart-line text-primary"></i>
+          Dashboard
+        </a>
 
-    <a href="users.html"
-      class="nav-link">
-      <i class="fa-solid fa-users text-primary"></i>
-      Users
-    </a>
+        <a href="users.html"
+          class="nav-link">
+          <i class="fa-solid fa-users text-primary"></i>
+          Users
+        </a>
 
-    <a href="departments.html"
-      class="nav-link">
-      <i class="fa-solid fa-building text-primary"></i>
-      Departments
-    </a>
+        <a href="departments.html"
+          class="nav-link">
+          <i class="fa-solid fa-building text-primary"></i>
+          Departments
+        </a>
 
-    <a href="courses.html"
-      class="nav-link">
-      <i class="fa-solid fa-book text-primary"></i>
-      Courses
-    </a>
+        <a href="courses.html"
+          class="nav-link">
+          <i class="fa-solid fa-book text-primary"></i>
+          Courses
+        </a>
 
-    <a href="schedules.html"
-      class="nav-link">
-      <i class="fa-solid fa-calendar-days text-primary"></i>
-      Schedules
-    </a>
+        <a href="schedules.html"
+          class="nav-link">
+          <i class="fa-solid fa-calendar-days text-primary"></i>
+          Schedules
+        </a>
 
-    <a href="attendance-reports.html"
-      class="nav-link">
-      <i class="fa-solid fa-file-export text-primary"></i>
-      Attendance Reports
-    </a>
+        <a href="attendance-reports.html"
+          class="nav-link">
+          <i class="fa-solid fa-file-export text-primary"></i>
+          Attendance Reports
+        </a>
 
-    <a href="#" class="logout-link">
-      <i class="fa-solid fa-right-from-bracket "></i>
-      Logout
-    </a>
-  </nav>
-</aside>
+        <a href="#" class="logout-link">
+          <i class="fa-solid fa-right-from-bracket "></i>
+          Logout
+        </a>
+      </nav>
+   
 
   `;
   // RENDER SIDEBAR
-  sidebarEl.innerHTML =
+  const finalDesign =
     role === "instructor"
       ? instructorSidebar
       : role === "admin"
       ? adminSidebar
       : studentSidebar;
 
-  activeCurrentPage();
-});
+  return finalDesign;
+};
 
 export const activeCurrentPage = function () {
   const curPage = window.location.href;
@@ -137,11 +147,14 @@ export const activeCurrentPage = function () {
     dashboard: ".dashboard-btn",
     notification: ".notification-btn",
     studentProfile: ".studentProfile-btn",
+    courseReports: ".course-btn",
+    instructorCourses: ".instructor-btn",
+    markAttendance: ".mark-btn",
   };
   for (const [key, value] of Object.entries(pageMap)) {
     if (curPage.includes(key)) {
       const btn = document.querySelector(value);
-      btn?.classList.add("font-bold");
+      btn?.classList.add("active");
       break;
     }
   }
