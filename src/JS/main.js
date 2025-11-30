ce1c4516cc8f8089e8bf7ebc0a7dcaea01c5e61c
Basantmohamed234 <basantmohamed19115677865433@gmail.com>
2025-11-30 02:28:36 +0200

    export excel

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,2 +1,10 @@
 import "../style.css";
 import { getAllUsers } from "./firebase/firebase-helper.js";
+
+//Add export excel from student table
+const exportBtn = document.getElementById("exportExcelBtn");
+exportBtn.addEventListener("click", () => {
+  const table = document.getElementById("attendanceTable");
+  const workbook = XLSX.utils.table_to_book(table, { sheet: "Attendance" });
+  XLSX.writeFile(workbook, "attendance-report.xlsx");
+});
